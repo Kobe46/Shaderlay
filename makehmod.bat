89ba2f847ff7b8c419ee2bf7effc621bef1c4f56
Kobe46 <Kobe46@gmail.com>
2017-03-07 20:16:44 -0700

    Add files via upload

---
+++ makehmod.bat
@@ -0,0 +1,31 @@
+@echo off
+set moddir=Shaderlay.hmod
+set modname=Shaderlay.hmod
+set outdir=output
+for %%x in (%*) do set outdir=%1
+
+cd %moddir%
+if not ERRORLEVEL 0 goto error
+if EXIST "%modname%" del /F /Q "%modname%"
+tar -czvf "%modname%" *
+if ERRORLEVEL 9009 goto notar
+if NOT ERRORLEVEL 0 goto error
+cd ..
+if NOT EXIST "%outdir%" md "%outdir%"
+if NOT ERRORLEVEL 0 goto error
+if EXIST "%outdir%\%modname%" del /F /S /Q "%outdir%\%modname%"
+if NOT ERRORLEVEL 0 goto error
+move /Y "%moddir%\%modname%" "%outdir%"
+if not ERRORLEVEL 0 goto error
+echo Done!
+goto end
+
+:notar
+echo Error %ERRORLEVEL%. Maybe you have not tar?
+goto end
+
+:error
+echo Error %ERRORLEVEL%
+goto end
+
+:end
